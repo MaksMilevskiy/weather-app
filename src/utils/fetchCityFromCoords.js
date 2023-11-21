254ac28ea00dc68bfc030574a0210cca510909cc
Maksym Milevskyi <max.milevski@gmail.com>
2023-11-21 22:36:43 +0200

    Https fix #2

--- src/utils/fetchCityFromCoords.js
+++ src/utils/fetchCityFromCoords.js
@@ -1,6 +1,6 @@
 const fetchCityFromCoords = async (lat, lon, locale) => {
     const API_KEY = import.meta.env.VITE_WEATHER_APP_KEY;
-    const url = `http://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lon}&appid=${API_KEY}&lang=${locale}`;
+    const url = `https://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lon}&appid=${API_KEY}&lang=${locale}`;
 
     try {
         const data = await fetch(url);
